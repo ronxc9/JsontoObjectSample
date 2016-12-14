cb9fae553a96d941a2b047a52ecf48f9336af202
ronxc <ronxc@DESKTOP-6FR7FAK>
2016-12-14 21:33:20 +1030

    Add project files.

---
+++ JsontoObject/JsonObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsontoObject
+{
+    public class JsonObject
+    { 
+        public string kind { get; set;  }
+        public List<string> columns { get; set; }
+        public List<List<string>> rows { get; set; }
+    }
+}
---
+++ JsontoObject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsontoObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*use this to get properties: http://json2csharp.com/         */
+
+            string json = @"{  ""kind"": ""fusiontables#sqlresponse"",  ""columns"": [   ""MMM_CLASS""  ],  ""rows"": [   [    ""4""   ]  ] }";
+            Console.WriteLine(json);
+            JsonObject jsonboj=  JsonConvert.DeserializeObject<JsonObject>(json);
+            Console.WriteLine(jsonboj.rows[0][0]);
+        }
+    }
+}
